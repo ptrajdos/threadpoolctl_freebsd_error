5382f2d634c72937f6f8fba4cfe5d135f7efb414
ptrajdos <pawel.trajdos@pwr.edu.pl>
2023-08-06 10:39:08 +0200

    1. MWE

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
 CC=clang
+EXPORT_DYNAMIC=-rdynamic
 .PHONY: all clean
 
 example4:example4.c
-	${CC} -Wl,--export-dynamic ./example4.c -o example4 -ldl -g -D_GNU_SOURCE
+	${CC}  ${EXPORT_DYNAMIC} $< -o $@ -ldl -g -D_GNU_SOURCE
 
 example3:example3.c
-	${CC} -Wl,--export-dynamic ./example3.c -o example3 -ldl -g -D_GNU_SOURCE
+	${CC} ${EXPORT_DYNAMIC}  $< -o $@ -ldl -g -D_GNU_SOURCE
+
+example2:example2.c
+	${CC} ${EXPORT_DYNAMIC} $< -g -o $@ -D_GNU_SOURCE
+
+mwe:mwe.c
+	${CC} ${EXPORT_DYNAMIC} $< -g -o $@ -D_GNU_SOURCE
---
+++ mwe.c
@@ -0,0 +1,51 @@
+#include <link.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+/**
+ * Compile via: 
+ * gcc -rdynamic mwe.c -o mwe -g -D_GNU_SOURCE
+ * or
+ * clang -rdynamic mwe.c -o mwe -g -D_GNU_SOURCE 
+*/
+static int callback(struct dl_phdr_info *info, size_t size, void *data) {
+	//A simple callback function
+ 	printf("Name: \"%s\" (%d segments)\n", info->dlpi_name, info->dlpi_phnum);
+ 	return 0;
+}
+
+typedef int (*lib_func)(int (*callback)(struct dl_phdr_info *, size_t, void *), void* data);
+
+int call_from_libc(){
+	/**
+	 * Calls dl_iterate_phdr indirectly by loading libc via dlopen
+	 * */
+	void     *handle  = NULL;
+	lib_func  func    = NULL;
+	handle = dlopen("/lib/libc.so.7",RTLD_NOW|RTLD_NOLOAD);// Put the name of your libc shared object
+	if (handle == NULL){
+		fprintf(stderr, "Unable to open lib: %s\n", dlerror());
+		return -1;
+	 }
+	func = dlsym(handle, "dl_iterate_phdr");
+
+	if (func == NULL) {
+		fprintf(stderr, "Unable to get symbol\n");
+		return -1;
+	}
+
+	func(callback, NULL);
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	printf("Direct call:\n");
+ 	dl_iterate_phdr(callback, NULL);//It works
+
+	printf("Indirect call:\n");
+ 	call_from_libc();//It causes 'ld-elf.so.1: Can't find module with TLS index 1'
+
+ 	exit(EXIT_SUCCESS);
+}
